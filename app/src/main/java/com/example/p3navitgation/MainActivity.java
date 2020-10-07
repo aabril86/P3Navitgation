fda90562996119ec3e0f900922d207c4db8bb4b1
abril <aabril@elpuig.xeill.net>
2020-10-07 20:25:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/p3navitgation/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.p3navitgation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.p3navitgation.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.actionGotoDrawerActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DrawerActivity.class));
+            }
+        });
+
+        binding.actionGotoBottomActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BottomActivity.class));
+            }
+        });
+
+        binding.actionGotoOptionsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OptionsActivity.class));
+            }
+        });
+
+        binding.actionGotoTabbedActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TabbedActivity.class));
+            }
+        });
+    }
+}
